dbdbeb244fd7976cd2fe76365fdae2e3eba421e0
bgdar <akunzero975@gmail.com>
2025-05-06 22:31:18 +0700

    golang+1

---
+++ golang/README.md
@@ -0,0 +1,16 @@
+
+# sub-string-tampa-berulang.go
+Diberikan sebuah tali s, carilah panjang tali terpanjang sub-stringtanpa karakter duplikat.
+Contoh 1:
+Masukan: s = "abcabcbb"
+ Keluaran: 3
+ Penjelasan: Jawabannya adalah "abc", dengan panjang 3.
+Contoh 2:
+
+Masukan: s = "bbbbb"
+ Keluaran: 1
+ Penjelasan: Jawabannya adalah "b", dengan panjang 1.
+<https://leetcode.com/problems/longest-substring-without-repeating-characters/description/>
+
+--- 
+
---
+++ golang/sub-string-tampa-berulang.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func lengthOfLongestSubstring(s string) (int,string,error) {
+	var panjang_str  = 0;
+
+	var chartOriginalSrt = []rune(s)
+	var chartContainer = []rune{}//simpan sub string yang tidak duplicat
+	
+	for _,chart1 := range chartOriginalSrt  {
+
+	
+		for index2 ,chart2 := range chartContainer {
+		if chart1 == chart2 {
+			chartContainer = chartContainer[index2+1:]
+			break
+		}
+	}
+	
+	chartContainer =append(chartContainer, chart1)
+	if len(chartContainer) > int(panjang_str) {
+		panjang_str = len(chartContainer)
+}
+}
+	return int(panjang_str),string(chartContainer),nil
+
+	
+}
+
+func main()  {
+	hasil,chart_str,err := lengthOfLongestSubstring("ini contohhhhhh")
+
+	
+	if err != nil {
+		fmt.Print("ada kesalahan : ",err)
+	}
+
+	fmt.Printf("panjang string %d dari substring %s\n",hasil,chart_str)
+}
