7baed61681673ab9866129665d188e167f4d1c41
bgdar <akunzero975@gmail.com>
2025-08-30 20:52:39 +0700

    add .gitignore

--- rust/target/debug/deps/rust-c010d728359f651e.d
+++
@@ -1,6 +0,0 @@
-/home/dar05/programer/problem-solving/rust/target/debug/deps/librust-c010d728359f651e.rmeta: src/main.rs src/two_amount.rs
-
-/home/dar05/programer/problem-solving/rust/target/debug/deps/rust-c010d728359f651e.d: src/main.rs src/two_amount.rs
-
-src/main.rs:
-src/two_amount.rs:
